bf67ad9921ef11ab18503801669ac94ce4e6bda0
Joemungandar <joemungandar95@gmail.com>
2020-04-14 21:34:21 +0200

    improved readability to Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,15 +18,10 @@ ggT
   :: Integer -- ^ number a 
   -> Integer -- ^ number b
   -> Integer -- ^ the greatest common divisor
-ggT a b = 
-  if b < 0
-    then ggT a (-b) 
-    else if a < 0
-      then ggT (-a) (b)
-      else if b == 0
-        then a
-        else if a == 0 || a == b
-          then b
-          else if a > b
-            then ggT (a-b) b
-            else ggT a (b-a)
+ggT a b 
+  | (b < 0) = ggT a (-b) 
+  | (a < 0) = ggT (-a) (b)
+  | (b == 0) = a
+  | (a == 0 || a == b) = b
+  | (a > b) =  ggT (a-b) b
+  | (a < b) =  ggT a (b-a)
